fff2c8c416e9daf16cb57514fc9ac385765396b7
gracilenes <gracilenes876@gmail.com>
2025-05-15 11:56:18 +0000

    atividade

--- app.py
+++ app.py
@@ -44,3 +44,61 @@ def login():
     
     return render_template('login.html')
 
+# Rota para Lista de Usuários Cadastrados
+@app.route('/lista_usuarios', methods=['GET'])
+def lista_usuarios():
+    usuarios = [
+        {'nome': 'João', 'email': 'joao@email.com'},
+        {'nome': 'Maria', 'email': 'maria@email.com'}
+    ]
+    return render_template('listar.html', usuarios=usuarios)
+
+# Rota Buscar usuário(pelo nome)
+@app.route('/buscar_usuario', methods=['GET', 'POST'])
+def buscar_usuario():
+    if request.method == 'POST':
+        # Obter os dados do formulário
+        nome = request.form['nome']
+        
+        print(f'Usuário buscado: Nome: {nome}')
+        return render_template('sucesso.html', nome=nome)
+    
+    return render_template('buscar.html')
+
+# Rota Alterar senha
+@app.route('/alterar_senha', methods=['GET', 'POST'])
+def alterar_senha():
+    if request.method == 'POST':
+        # Obter os dados do formulário
+        email = request.form['email']
+        nova_senha = request.form['nova_senha']
+        
+        print(f'Senha alterada: Email: {email}, Nova Senha: {nova_senha}')
+        return render_template('sucesso.html', nome=email)
+    
+    return render_template('alterar_senha.html')
+
+# Rota para recurar senha
+@app.route('/recuperar_senha', methods=['GET', 'POST'])
+def recuperar_senha():
+    if request.method == 'POST':
+        # Obter os dados do formulário
+        email = request.form['email']
+        
+        print(f'Senha recuperada: Email: {email}')
+        return render_template('sucesso.html', nome=email)
+    
+    return render_template('recuperar_senha.html')
+
+# Rota para  função extra definida pelo aluno
+@app.route('/funcao_extra', methods=['GET', 'POST'])
+def funcao_extra():
+    if request.method == 'POST':
+        # Obter os dados do formulário
+        nome = request.form['nome']
+        email = request.form['email']
+        
+        print(f'Função extra: Nome: {nome}, Email: {email}')
+        return render_template('sucesso.html', nome=nome)
+    
+    return render_template('funcao_extra.html')
